985779203cf573c2896d3ebb720aa780519634e3
Enzo <enzo.e.vignolo@gmail.com>
2021-02-17 22:35:46 +0100

    Changed the whole design and layout

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log(document.getElementsByClassName("studies"));
 studiesDetail = document.getElementsByClassName("studies");
 experienceDetail = document.getElementsByClassName("experience");
 coursesDetail = document.getElementsByClassName("courses");
@@ -5,38 +6,45 @@ portfolioDetail = document.getElementsByClassName("portfolio");
 contactDetail = document.getElementsByClassName("contact");
 for (i in [...Array(2).keys()]) {
   studiesDetail[i].addEventListener("click", () => {
-    showDetails("studies-detail");
+    showDetails("studies-detail", "arrow-1");
   });
 }
 for (i in [...Array(2).keys()]) {
   experienceDetail[i].addEventListener("click", () => {
-    showDetails("work-detail");
+    showDetails("work-detail", "arrow-2");
   });
 }
 for (i in [...Array(2).keys()]) {
   coursesDetail[i].addEventListener("click", () => {
-    showDetails("courses-detail");
+    showDetails("courses-detail", "arrow-3");
   });
 }
 for (i in [...Array(2).keys()]) {
   portfolioDetail[i].addEventListener("click", () => {
-    showDetails("portfolio-detail");
+    showDetails("portfolio-detail", "arrow-4");
   });
 }
 for (i in [...Array(2).keys()]) {
   contactDetail[i].addEventListener("click", () => {
-    showDetails("contact-detail");
+    showDetails("contact-detail", "arrow-5");
   });
 }
 
-function showDetails(elementDetail) {
+function showDetails(elementDetail, arrow) {
+  console.log(elementDetail);
   element = document.getElementById(elementDetail);
+  console.log(element);
   elementStyle = getComputedStyle(element);
-  if (elementStyle.maxHeight == "0px") {
-    element.style.maxHeight = "1000px";
-    element.style.transistion = "max-height 2s";
+  if (elementStyle.maxWidth == "0%") {
+    element.style.maxHeight = "500rem";
+    element.style.maxWidth = "35%";
+    element.style.transistion = "max-width 2s ,max-height 2s";
+    arrowElement = document.getElementById(arrow);
+    arrowElement.style.transform = "rotate(0.25turn)";
   } else {
-    element.style.maxHeight = "0px";
-    element.style.transition = "max-height 0.5s";
+    element.style.maxHeight = "0rem";
+    element.style.maxWidth = "0%";
+    element.style.transition = "max-width 0.5s, max-height 0.5s";
+    arrowElement.style.transform = "rotate(-0.25turn)";
   }
 }
